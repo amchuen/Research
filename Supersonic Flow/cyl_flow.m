e0f70baaf775e1459c8c71900b8ee5b2527d20b7
Andrew Chuen <amchuen@ucdavis.edu>
2017-02-24 16:06:10 -0800

    Moved Algorithm Over to MATLAB Function

--- Supersonic Flow/cyl_flow.m
+++ Supersonic Flow/cyl_flow.m
@@ -7,7 +7,6 @@ close all;
 % Step Sizes
 dr = 0.1;
 dT = 0.01;
-% dt = 0.1*dr*dT;
 
 % Cylinder Dimensions
 r_cyl = 1.0;
@@ -17,246 +16,31 @@ r_max = 3;
 r_vals = r_cyl:dr:(r_max+dr);
 T_vals = 0:dT:(pi);
 
-
-dts = [0.1, 0.05, 0.01]*dr*dT;
-M_vals = [0];
-gam = 1.4;
-
 % Build Field
-[TT, RR] = meshgrid(T_vals, r_vals);
+[grid.TT, grid.RR] = meshgrid(T_vals, r_vals);
+grid.dT = dT;
+grid.dr = dr;
 
-XX = RR .* cos(TT);
-YY = RR .* sin(TT);
+grid.XX = grid.RR .* cos(grid.TT);
+grid.YY = grid.RR .* sin(grid.TT);
 
-% PHI = zeros(length(r_vals), length(T_vals), length(t_vals));
+% Initialize flow properties
+flow.gamma = 1.4;
+flow.M0 = 0.0;
 
-% Define the density equation
+% Initialize time grids
+times.start = 0.0; % seconds
+times.dt = 0.1*dr*dT;
+% times.dts = [0.1, 0.05, 0.01]*dr*dT;
+times.stop = 100 * times.dt;
+times.alpha = 10/(times.dt);
+
+%% Run Solver
+
+[PHI, PHI_R, PHI_T, RHO] = flowSolve_comp(grid, flow, times);
 
 
 %% Boundary Conditions
-
-% PHI(:,:,1) = RR.*cos(TT) + cos(TT)./RR;
-% PHI(:,:,2) = PHI(:,:,1);
-
-PHI_T1 = ones(length(M_vals), length(T_vals));
-PHI_RHO1 = ones(size(PHI_T1));
-res_time = {};
-
-for tt = 1:length(dts)
-    t_vals = 0:dts(tt):(100*dts(tt));
-    dt = dts(tt);
-    alpha = 10/(dt);
-    res = ones(size(t_vals));
-    PHI = zeros(length(r_vals), length(T_vals), length(t_vals));
-    for mm = 1:length(M_vals)
-        M0 = M_vals(1);
-        PHI(:,:,1) = RR.*cos(TT) + cos(TT)./RR;
-        PHI(:,:,2) = PHI(:,:,1);
-        for n = 3:length(t_vals) % loop through time
-            for i = 1:length(T_vals) % loop through theta        
-                for j = 1:(length(r_vals)-1) % loop through radius
-                    if j ~= (length(r_vals)-1) % only run this if not in the boundary condition
-                        % 5 different control indexes for i and j directions            
-                        i0 = i;
-                        j0 = j;
-                        % Boundary Conditions for Theta
-                        if i == 1
-                            i1 = i+1;
-                            i2 = i+2;
-                            i_1 = i1;
-                            i_2 = i2;
-                        elseif i == 2
-                            i1 = i+1;
-                            i2 = i+2;
-                            i_1 = i-1;
-                            i_2 = i;
-                        elseif i == length(T_vals)
-                            i0 = i;
-                            i1 = i-1;
-                            i2 = i-2;
-                            i_1 = i-1;
-                            i_2 = i-2;
-                        elseif i == length(T_vals)-1
-                            i1 = i+1;
-                            i2 = i;
-                            i_1 = i-1;
-                            i_2 = i-2;
-                        else
-                            i1 = i+1;
-                            i2 = i+2;
-                            i_1 = i-1;
-                            i_2 = i-2;
-                        end
-
-                        % Boundary Conditions for Radius
-                        if j == 1
-                            j1 = j+1;
-                            j2 = j+2;
-                            j_1 = j1;
-                            j_2 = j2;
-                            n1 = n-1; % use with j_1
-                            n2 = n-1; % use with j_2
-                        elseif j ==2
-                            j1 = j+1;
-                            j2 = j+2;
-                            j_1 = j-1;
-                            j_2 = j;
-                            n1 = n-1; % use with j_1
-                            n2 = n-1; % use with j_2
-                        elseif j == length(r_vals)-3
-                            j1 = j+1;
-                            j2 = j+2;
-                            j_1 = j-1;
-                            j_2 = j-2;
-                            n1 = n-1; % use with j_1
-                            n2 = n-1; % use with j_2
-                        elseif j == length(r_vals)-2
-                            j1 = j+1;
-                            j2 = j;
-                            j_1 = j-1;
-                            j_2 = j-2;
-                            n1 = n-1; % use with j_1
-                            n2 = n-1; % use with j_2
-                        else
-                            j1 = j+1;
-                            j2 = j+2;
-                            j_1 = j-1;
-                            j_2 = j-2;
-                            n1 = n-1; % use with j_1
-                            n2 = n-1; % use with j_2
-                        end
-
-                        % RHO(j,i)
-                        RHO_ij = (1 - 0.5*(gam-1)*M0^2*(((PHI(j1, i0, n-1) - PHI(j_1, i0, n1))/(2*dr))^2 + (1/RR(j0,i0))^2*((PHI(j0, i1, n-1)-PHI(j0,i_1,n-1))/(2*dT))^2 + 2*(PHI(j0,i0,n-1) - PHI(j0,i0,n-2))/dt - 1))^(1/(gam-1));
-
-                        % RHO(j+1, i)
-                        RHO_ij1 = (1 - 0.5*(gam-1)*M0^2*(((PHI(j2, i0, n-1) - PHI(j0, i0, n-1))/(2*dr))^2 + (1/RR(j1,i0))^2*((PHI(j1, i1, n-1)-PHI(j1,i_1,n-1))/(2*dT))^2 + 2*(PHI(j1,i0,n-1) - PHI(j1,i0,n-2))/dt -1))^(1/(gam-1));
-
-                        % RHO(j-1, i)
-                        RHO_ij0 = (1 - 0.5*(gam-1)*M0^2*(((PHI(j0, i0, n-1) - PHI(j_2, i0, n2))/(2*dr))^2 + (1/RR(j_1,i0))^2*((PHI(j_1, i1, n1)-PHI(j_1,i_1,n1))/(2*dT))^2 + 2*(PHI(j_1,i0,n1) - PHI(j_1,i0,n1-1))/dt - 1))^(1/(gam-1));
-
-                        % RHO(j,i+1)
-                        RHO_i1j = (1 - 0.5*(gam-1)*M0^2*(((PHI(j1, i1, n-1) - PHI(j_1, i1, n1))/(2*dr))^2 + (1/RR(j0,i1))^2*((PHI(j0, i2, n-1)-PHI(j0,i0,n-1))/(2*dT))^2 + 2*(PHI(j0,i1,n-1) - PHI(j0,i1,n-2))/dt -1))^(1/(gam-1));
-
-                        % RHO(j,i-1)
-                        RHO_i0j = (1 - 0.5*(gam-1)*M0^2*(((PHI(j1, i_1, n-1) - PHI(j_1, i_1, n1))/(2*dr))^2 + (1/RR(j0,i_1))^2*((PHI(j0, i0, n-1)-PHI(j0,i_2,n-1))/(2*dT))^2 + 2*(PHI(j0,i_1,n-1) - PHI(j0,i_1,n-2))/dt -1))^(1/(gam-1));
-
-                        % RHO(j+1,i+1)
-                        RHO_i1j1 = (1 - 0.5*(gam-1)*M0^2*(((PHI(j2, i1, n-1) - PHI(j0, i1, n-1))/(2*dr))^2 + (1/RR(j1,i1))^2*((PHI(j1, i2, n-1)-PHI(j1,i0,n-1))/(2*dT))^2 + 2*(PHI(j1,i1,n-1) - PHI(j1,i1,n-2))/dt -1))^(1/(gam-1));
-
-                        % RHO(j+1,i-1)
-                        RHO_i0j1 = (1 - 0.5*(gam-1)*M0^2*(((PHI(j2, i_1, n-1) - PHI(j0, i_1, n-1))/(2*dr))^2 + (1/RR(j1,i_1))^2*((PHI(j1, i0, n-1)-PHI(j1,i_2,n-1))/(2*dT))^2 + 2*(PHI(j1,i_1,n-1) - PHI(j1,i_1,n-2))/dt -1))^(1/(gam-1));
-
-                        % RHO(j-1,i+1)
-                        RHO_i1j0 = (1 - 0.5*(gam-1)*M0^2*(((PHI(j0, i1, n-1) - PHI(j_2, i1, n2))/(2*dr))^2 + (1/RR(j_1,i1))^2*((PHI(j_1, i2, n1)-PHI(j_1,i0,n1))/(2*dT))^2 + 2*(PHI(j_1,i1,n1) - PHI(j_1,i1,n1-1))/dt -1))^(1/(gam-1));
-
-                        % RHO(j-1,i-1)
-                        RHO_i0j0 = (1 - 0.5*(gam-1)*M0^2*(((PHI(j0, i_1, n-1) - PHI(j_2, i_1, n2))/(2*dr))^2 + (1/RR(j_1,i_1))^2*((PHI(j_1, i0, n1)-PHI(j_1,i_2,n1))/(2*dT))^2 + 2*(PHI(j_1,i_1,n1) - PHI(j_1,i_1,n1-1))/dt -1))^(1/(gam-1));
-
-                        % RHO(j, i-2) - (forward difference)
-                        RHO_i_1j = (1 - 0.5*(gam-1)*M0^2*(((PHI(j1, i_2, n-1) - PHI(j_1, i_2, n1))/(2*dr))^2 + (1/RR(j0,i_2))^2*((PHI(j0, i_1, n-1)-PHI(j0,i_2,n-1))/(dT))^2 + 2*(PHI(j0,i_2,n-1) - PHI(j0,i_2,n-2))/dt -1))^(1/(gam-1));
-
-                        % RHO(j-2, i) - (forward difference)
-                        RHO_ij_1 = (1 - 0.5*(gam-1)*M0^2*(((PHI(j_1, i0, n-1) - PHI(j_2, i0, n2))/(dr))^2 + (1/RR(j_2,i0))^2*((PHI(j_2, i1, n2)-PHI(j_2,i_1,n2))/(2*dT))^2 + 2*(PHI(j_2,i0,n) - PHI(j_2,i0,n-1))/dt - 1))^(1/(gam-1));
-
-                        % RHO_S(j,i)
-                        PHI_Tij = (PHI(j0,i1, n-1) - PHI(j0,i_1, n-1))/(2*dT);
-                        PHI_Rij = (PHI(j1,i0,n-1)-PHI(j_1,i0,n1))/(2*dr);
-                        a2_ij = 2/(M0^2) - (gam-1) * (PHI_Rij^2 + ((1/RR(j0,i0))*PHI_Tij)^2 - 1 );
-                        M_ij = (((1/RR(j0,i0))*PHI_Tij)^2 + PHI_Rij^2)/(a2_ij); % calculate local Mach number
-                        e_ij = max([0.0, 1-(a2_ij/(M_ij^2))]); % artificial viscosity
-                        RHO_Sij = e_ij*(PHI_Tij*(RHO_ij - RHO_i0j) + PHI_Rij*(RHO_ij - RHO_ij0));
-
-                        % RHO_S(j+1,i)
-                        PHI_Tij1 = (PHI(j1,i1, n-1) - PHI(j1,i_1, n-1))/(2*dT);
-                        PHI_Rij1 = (PHI(j2,i0,n-1)-PHI(j0,i0,n-1))/(2*dr);
-                        a2_ij1 = 2/(M0^2) - (gam-1) * (PHI_Rij1^2 + ((1/RR(j1,i0))*PHI_Tij1)^2 - 1 );
-                        M_ij1 = (((1/RR(j1,i1))*PHI_Tij1)^2 + PHI_Rij1^2)/(a2_ij1);
-                        e_ij1 = max([0.0, 1-(a2_ij1/(M_ij1^2))]);
-                        RHO_Sij1 = e_ij1*(PHI_Tij1*(RHO_ij - RHO_i0j) + PHI_Rij1*(RHO_ij - RHO_ij0));
-
-                        % RHO_S(j-1,i)
-                        PHI_Tij0 = (PHI(j_1,i1, n1) - PHI(j_1,i_1, n1))/(2*dT);
-                        PHI_Rij0 = (PHI(j0,i0,n-1)-PHI(j_2,i0,n2))/(2*dr);
-                        a2_ij0 = 2/(M0^2) - (gam-1) * (PHI_Rij0^2 + ((1/RR(j_1,i0))*PHI_Tij0)^2 - 1 );
-                        M_ij0 = (((1/RR(j_1,i0))*PHI_Tij0)^2 + PHI_Rij0^2)/(a2_ij0);
-                        e_ij0 = max([0.0, 1-(a2_ij0/(M_ij0^2))]);
-                        RHO_Sij0 = e_ij0*(PHI_Tij0*(RHO_ij0 - RHO_i0j0) + PHI_Rij0*(RHO_ij0 - RHO_ij_1)); % need j-2
-
-                        % RHO_S(j,i+1), j = j0, i = i1
-                        PHI_Ti1j = (PHI(j0,i2, n-1) - PHI(j0,i0, n-1))/(2*dT);
-                        PHI_Ri1j = (PHI(j1,i1,n-1)-PHI(j_1,i1,n1))/(2*dr);
-                        a2_i1j = 2/(M0^2) - (gam-1) * (PHI_Ri1j^2 + ((1/RR(j0,i1))*PHI_Ti1j)^2 - 1 );
-                        M_i1j = (((1/RR(j0,i1))*PHI_Ti1j)^2 + PHI_Ri1j^2)/(a2_i1j);
-                        e_i1j = max([0.0, 1-(a2_i1j/(M_i1j^2))]);
-                        RHO_Si1j = e_i1j*(PHI_Ti1j*(RHO_i1j - RHO_ij) + PHI_Ri1j*(RHO_i1j - RHO_i1j0));
-
-                        % RHO_S(j,i-1)
-                        PHI_Ti0j = (PHI(j0,i0, n-1) - PHI(j0,i_2, n-1))/(2*dT);
-                        PHI_Ri0j = (PHI(j1,i_1,n-1)-PHI(j_1,i_1,n1))/(2*dr);
-                        a2_i0j = 2/(M0^2) - (gam-1) * (PHI_Ri0j^2 + ((1/RR(j0,i_1))*PHI_Ti0j)^2 - 1 );
-                        M_i0j = (((1/RR(j0,i_1))*PHI_Ti0j)^2 + PHI_Ri0j^2)/(a2_i0j);
-                        e_i0j = max([0.0, 1-(a2_i0j/(M_i0j^2))]);
-                        RHO_Si0j = e_i0j*(PHI_Ti0j*(RHO_i0j - RHO_i_1j) + PHI_Ri0j*(RHO_i0j - RHO_i0j0)); % need RHO(i-2, j)
-
-                        % Density averages
-                        RHO_j0 = 0.5*(RHO_ij + RHO_ij0 - RHO_Sij - RHO_Sij0); % (i, j-0.5)
-                        RHO_j1 = 0.5*(RHO_ij + RHO_ij1 - RHO_Sij - RHO_Sij1); % (i, j+0.5)
-                        RHO_i0 = 0.5*(RHO_ij + RHO_i0j - RHO_Sij - RHO_Si0j); % (i-0.5, j)
-                        RHO_i1 = 0.5*(RHO_ij + RHO_i1j - RHO_Sij - RHO_Si1j); % (i+0.5, j)
-
-                        % Second Order Derivatives
-                        PHI_TT = (1/RR(j0,i0)^2)*(1/dT)*(RHO_i1*(PHI(j0,i1,n-1)-PHI(j0,i0,n-1))/dT - RHO_i0*(PHI(j0,i0,n-1)-PHI(j0,i_1,n-1))/dT);
-                        PHI_RR = (1/RR(j0,i0))*((0.5*(RR(j1,i0)+RR(j0,i0))*(PHI(j1,i0,n-1)-PHI(j0,i0,n-1))/dr)*RHO_j1 - RHO_j0*(0.5*(RR(j0,i0)+RR(j_1,i0))*(PHI(j0,i0,n-1)-PHI(j_1,i0,n-1))/dr))/dr;
-
-                        % Apply Conditions for Time
-                        PHI(j0,i0,n) = ((0.5*alpha/dt - 1/dt^2)*PHI(j0,i0,n-2) + 2*PHI(j0,i0,n-1)/dt^2 + PHI_RR + PHI_TT)/(1/dt^2 + 0.5*alpha/dt);
-                    else
-                        PHI(j,i,n) = PHI(j,i,n-1); % set the boundary condition
-                    end
-                end
-            end
-            difference = abs(PHI(:,:,n) - PHI(:,:,n-1));
-            res(n-2) = max(difference(:));
-        end
-
-            %% Generate Plots
-
-        % Uvals = size(PHI(:,
-
-%         figure();
-%         contourf(XX, YY, PHI(:,:,length(t_vals)));
-
-        PHI_R = zeros(size(PHI));
-        PHI_T = zeros(size(PHI));
-        for n = 1:length(t_vals)
-            for j = 2:length(r_vals)
-               PHI_R(j,:,n) =  (PHI(j,:,n)-PHI(j-1,:,n))./(RR(j,:) - RR(j-1,:));
-            end
-
-            for i = 1:length(T_vals)
-                i0 = i;
-                if i == 1
-                    i1 = i+1;
-                    i_1 = length(T_vals);
-                elseif i == length(T_vals)
-                    i1 = 1;
-                    i_1 = i-1;
-                else
-                    i1 = i+1;
-                    i_1 = i-1;
-                end 
-
-                PHI_T(:,i0,n) =  (1/RR(:,i0))*0.5*(PHI(:,i1,n)-PHI(:,i_1,n))./(TT(:,i1) - TT(:,i_1));
-            end
-        end
-        PHI_T1(mm,:) = PHI_T(1,:,end);
-        PHI_RHO = (1 - 0.5.*(gam-1).*M0^2.*(PHI_R(:,:,end).^2 + (PHI_T(:,:,end)./RR(:,:)).^2 - 1)).^(1/(gam-1));
-        PHI_RHO1(mm,:) = PHI_RHO(1,:,end);
-    end
-    res_time{tt} = [t_vals(1:end-2); res(1:end-2)];
-end
-
 
 figure();
 contourf(XX, YY, 1-((PHI_R(:,:,end).^2 + PHI_T(:,:,end).^2)), 50); %./((RR.*cos(TT)).^2)
@@ -267,27 +51,3 @@ for mm = 1:length(M_vals)
     plot(T_vals, PHI_T1(mm,:));
     hold on;
 end
-
-
-
-% figure();
-% quiver(XX,YY, PHI_)
-
-% figure();
-% contourf(XX, YY, (PHI_R(:,:,1).^2 + PHI_T(:,:,1).^2).^0.5);
-
-%% Plot Density
-
-% PHI_RHO = (1 - 0.5*(gam-1)*(PHI_R(:,:,end)^2 + (PHI_T(:,:,end)/RR(:,:))^2 - 1))^(1/(gam-1));
-strtype = {'--', 'o'};
-figure();
-for mm = 1:length(M_vals)
-    plot(T_vals, PHI_RHO1(mm,:), strtype{mm});
-    hold on;
-end
-
-figure(); loglog(res_time{1}(1,:), res_time{1}(2,:));
-hold on;
-loglog(res_time{2}(1,:), res_time{2}(2,:), 'o');
-hold on;
-loglog(res_time{3}(1,:), res_time{3}(2,:), 'o');
---
+++ Supersonic Flow/flowSolve_comp.m
@@ -0,0 +1,139 @@
+function [PHI, PHI_R, PHI_T, RHO] = flowSolve_comp(grid, flow, times)
+
+% Initilize Grid
+dr = grid.dr;
+dT = grid.dT;
+dt = times.dt;
+RR = grid.RR;
+TT = grid.TT;
+
+% initalize grid count
+n_t = (times.stop - times.start)/dt;
+[n_r, n_T] = size(grid.RR);
+
+% Initialize Values for flow field
+PHI = zeros(n_r, n_T, n_t);
+PHI(:,:,1) = RR.*cos(TT) + cos(TT)./RR;
+PHI(:,:,2) = PHI(:,:,1);
+
+% Initialize flow field derivatives
+RHO = zeros(size(PHI));
+PHI_R = zeros(size(PHI));
+PHI_T = zeros(size(PHI));
+% PHI_t = zeros(size(PHI));
+
+% Begin iteration
+for n = 3:n_t % loop through time
+    
+    % Update derivatives
+    [PHI_R(:,:,n-1), PHI_T(:,:,n-1), RHO(:,:,n-1)] = update_derivs(PHI, RR, TT, flow.M0, flow.gamma);
+    
+    for i = 1:n_T % loop through theta        
+        for j = 2:(n_r-1) % loop through radius
+            % 5 different control indexes for i and j directions            
+            i0 = i;
+            j0 = j;
+            % Boundary Conditions for Theta
+            if i == 1
+                i1 = i+1;
+                i_1 = i1;
+            elseif i == n_T
+                i0 = i;
+                i1 = i-1;
+                i_1 = i-1;
+            else
+                i1 = i+1;
+                i_1 = i-1;
+            end
+
+            % Boundary Conditions for Radius
+            if j == 1
+                j1 = j+1;
+                j_1 = j1;
+            elseif j == n_r
+                j1 = j+1;
+                j_1 = j-1;
+            else
+                j1 = j+1;
+                j_1 = j-1;
+            end
+            
+            % Density averages
+            RHO_i1 = 0.5*(RHO(j0,i1) + RHO(j0,i0));
+            RHO_i0 = 0.5*(RHO(j0,i_1) + RHO(j0,i0));
+            RHO_j1 = 0.5*(RHO(j1, i0) + RHO(j0,i0));
+            RHO_j0 = 0.5*(RHO(j_1, i0) + RHO(j0,i0));
+            
+            % Second Order Derivatives
+            PHI_TT = (1/RR(j0,i0)^2)*(1/dT)*(RHO_i1*(PHI(j0,i1,n-1)-PHI(j0,i0,n-1))/dT - RHO_i0*(PHI(j0,i0,n-1)-PHI(j0,i_1,n-1))/dT);
+            PHI_RR = (1/RR(j0,i0))*((0.5*(RR(j1,i0)+RR(j0,i0))*(PHI(j1,i0,n-1)-PHI(j0,i0,n-1))/dr)*RHO_j1 - RHO_j0*(0.5*(RR(j0,i0)+RR(j_1,i0))*(PHI(j0,i0,n-1)-PHI(j_1,i0,n-1))/dr))/dr;
+
+            % Apply Conditions for Time
+            PHI(j0,i0,n) = ((0.5*alpha/dt - 1/dt^2)*PHI(j0,i0,n-2) + 2*PHI(j0,i0,n-1)/dt^2 + PHI_RR + PHI_TT)/(1/dt^2 + 0.5*alpha/dt);
+        end
+        PHI(j,i,n) = PHI(j,i,n-1); % set the boundary condition
+    end
+end
+
+function [phi_r, phi_th, rho] = update_derivs(phi, rr, tt, M0, gam)
+
+% initialize temp vals for PHI_R, PHI_T, and rho
+phi_r = zeros(size(phi(:,:,n-1)));
+phi_th = zeros(size(phi(:,:,n-1)));
+rho = zeros(size(phi(:,:,n-1)));
+% phi_t = zeros(size(rho));
+
+    % Radial derivative
+    for jj = 2:n_r
+       phi_r(jj,:) =  (phi(jj,:,n-1)-phi(jj-1,:,n-1))./(rr(jj,:) - rr(jj-1,:));
+    end
+
+    phi_r(1,:) = zeros(1,n_T);
+
+    % Angular derivative
+    for ii = 1:n_T
+        ii0 = ii;
+        if ii == 1
+            ii1 = ii+1;
+            ii_1 = n_T;
+        elseif ii == n_T
+            ii1 = 1;
+            ii_1 = ii-1;
+        else
+            ii1 = ii+1;
+            ii_1 = ii-1;
+        end 
+
+        phi_th(:,ii0,n-1) =  (1/rr(:,ii0))*0.5*(phi(:,ii1,n)-phi(:,ii_1,n))./(tt(:,ii1) - tt(:,ii_1));
+    end
+
+    % Time Derivative
+    phi_t = (phi(:,:,n-1) - phi(:,:,n-2))/dt;
+
+    % Local speed of sound
+    a2_ij = (2/(M0^2) - (gam - 1).*(phi_r.^2 + (phi_th./rr).^2 - 1));
+
+    % Local Mach number & artificial viscosity
+    M_ij = (((phi_th./rr).^2 + phi_r.^2)./a2_ij).^2;
+    eps = max(zeros(size(M_ij)), 1 - 0.9./(M_ij.^2));
+
+    % Local density calculations
+    rho_bar = (1 - 0.5.*(gam-1).*M0^2.*(phi_r.^2 + (phi_th./rr).^2 + 2*phi_t - 1)).^(1/(gam-1));
+
+    for ii = 1:n_T % loop through Thetas
+        ii0 = ii;
+        if ii == 1
+            ii_1 = n_T;
+        else
+            ii_1 = ii-1;
+        end
+        for jj = 2:n_r % Loop through Radii
+            rho(jj,ii0) = rho_bar(jj, ii0) - eps(jj, ii0)*(phi_th(jj, ii0)*(rho_bar(jj,ii0) - rho_bar(jj,ii_1)) + phi_r(jj,ii0)*(rho_bar(jj,ii0) - rho_bar(jj-1, ii0)));
+        end
+
+        rho(1,ii0) = rho_bar(1,ii0) - eps(1,ii0) * (phi_th(1,ii0) * (rho_bar(1,ii0) - rho_bar(1, ii_1))); % all PHI_R's at surface are zero
+    end
+end
+
+end
+
