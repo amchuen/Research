2d28715ff31e85ac74a2954307eaf16cefcd83e0
Andrew Chuen <amchuen@ucdavis.edu>
2017-02-28 19:12:31 -0800

    Fixed changes with book-keeping density.

--- Supersonic Flow/cyl_flow.m
+++ Supersonic Flow/cyl_flow.m
@@ -5,8 +5,9 @@ close all;
 %% SIM CONTROL PARAMS
 
 % Step Sizes
-dr = 0.01;
-dT = 0.01;
+dr = 0.1;
+dT = 0.01*pi;
+dt = 0.1*dr;
 
 % Cylinder Dimensions
 r_cyl = 1.0;
@@ -19,7 +20,7 @@ T_vals = 0:dT:(pi);
 % Time Inputs
 start = 0.0;
 % stop = 100 * run.times.dt;
-dt = [0.1]*dr*dT;
+% dt = [0.001];
 
 
 %% GRID SETUP
@@ -35,22 +36,29 @@ run.grid.YY = run.grid.RR .* sin(run.grid.TT);
 
 % Initialize flow properties
 run.flow.gamma = 1.4;
-run.flow.M0 = 0.0;
+run.flow.PHI_INIT = run.grid.XX; 
+M0 = [0.0, 0.2, 0.3, 0.4];
 
-%% Loop and Solve for Convergenc w/ Time
+%% Loop and Solve for Convergence w/ Time
 
 resCheck = figure();
+title('Residual Check');
+theta = figure();
 % Initialize time grids
 run.times.start = start; % seconds
-for nn = 1:length(dt)
+for nn = 1:length(M0)
     fprintf('Simulation %i Started...\n', nn);
     if nn ~= 1
        run(nn) = run(1);
     end
     
-    run(nn).times.dt = dt(nn);
-    run(nn).times.alpha = 10/dt(nn);
-    run(nn).times.stop = 100 * dt(nn);
+    run(nn).flow.M0 = M0(nn);
+    if nn > 1
+       run(nn).flow.PHI_INIT = run(nn-1).sol.PHI(:,:,end); 
+    end
+    run(nn).times.dt = dt;
+    run(nn).times.alpha = 5*1.5;%10/dt(nn);
+    run(nn).times.stop = 202 * dt;
 
     % Run Solver
     [run(nn).sol.PHI, run(nn).sol.PHI_R, run(nn).sol.PHI_T, run(nn).sol.RHO, run(nn).sol.res] = flowSolve_comp(run(nn).grid, run(nn).flow, run(nn).times);
@@ -60,28 +68,31 @@ for nn = 1:length(dt)
     % Plot Residuals
     figure(resCheck);
     hold on;
-    loglog(0:1:(100-3), run(nn).sol.res);
+    plot((0:1:((run(nn).times.stop - run(nn).times.start)/run(nn).times.dt - 3)), log10(run(nn).sol.res));
     hold off;
     
     % Plot Fields
     PHI_X = run(nn).sol.PHI_R(:,:,end).*cos(run(nn).grid.TT) - run(nn).sol.PHI_T(:,:,end).*sin(run(nn).grid.TT);
     PHI_Y = run(nn).sol.PHI_R(:,:,end).*sin(run(nn).grid.TT) + run(nn).sol.PHI_T(:,:,end).*cos(run(nn).grid.TT);
-    figure(); % cp plots
-    contourf(run(nn).grid.XX, run(nn).grid.YY, 1-(PHI_X.^2 + PHI_Y.^2), 50); %./((RR.*cos(TT)).^2)
-    title(sprintf('Pressure Coefficient Contours (Sim %i)', nn));
-    colorbar('eastoutside');
+%     figure(); % cp plots
+%     contourf(run(nn).grid.XX, run(nn).grid.YY, 1-(PHI_X.^2 + PHI_Y.^2), 50); %./((RR.*cos(TT)).^2)
+%     title(sprintf('Pressure Coefficient Contours (Sim %i)', nn));
+%     colorbar('eastoutside');
     
-    figure(); % field potential
-    contourf(run(nn).grid.XX, run(nn).grid.YY, run(nn).sol.PHI(:,:,end), 50);
-    title(sprintf('Field Potential (Sim %i)', nn));
-    colorbar('eastoutside');
-    axis equal
+%     figure(); % field potential
+%     contourf(run(nn).grid.XX, run(nn).grid.YY, run(nn).sol.PHI(:,:,end), 50);
+%     title(sprintf('Field Potential (Sim %i)', nn));
+%     colorbar('eastoutside');
+%     axis equal
     
-    figure(); % x-dir velocity plots
-    contourf(run(nn).grid.XX, run(nn).grid.YY, PHI_X, 50); %./((RR.*cos(TT)).^2)
-    title(sprintf('U velocity (Sim %i)', nn));
-    colorbar('eastoutside');
-    
+%     figure(); % x-dir velocity plots
+%     contourf(run(nn).grid.XX, run(nn).grid.YY, PHI_X, 50); %./((RR.*cos(TT)).^2)
+%     title(sprintf('U velocity (Sim %i)', nn));
+%     colorbar('eastoutside');
+    figure(theta);
+    hold on;
+    plot(run(nn).grid.TT(1,:), run(nn).sol.PHI_T(1,:,end));
+    hold off;
 end
 
 
--- Supersonic Flow/flowSolve_comp.m
+++ Supersonic Flow/flowSolve_comp.m
@@ -14,14 +14,18 @@ n_t = (times.stop - times.start)/dt;
 
 % Initialize Values for flow field
 PHI = zeros(n_r, n_T, n_t);
-PHI(:,:,1) = RR.*cos(TT);
+PHI(:,:,1) = flow.PHI_INIT;
 PHI(:,:,2) = PHI(:,:,1);
+
+% Boundary Conditions for field at time n
+PHI_BC = RR(n_r,:).*cos(TT(n_r,:)) + cos(TT(n_r,:))./RR(n_r,:);
+% PHI_cyl = RR(1,:).*cos(TT(1,:)) + cos(TT(1,:))./RR(1,:);
 
 % Initialize flow field derivatives
 RHO = zeros(size(PHI));
 PHI_R = zeros(size(PHI));
 PHI_T = zeros(size(PHI));
-[PHI_R(:,:,2), PHI_T(:,:,2), RHO(:,:,2)] = update_derivs(PHI, RR, TT, flow.M0, flow.gamma);
+[PHI_R(:,:,2), PHI_T(:,:,2), RHO(:,:,2)] = update_rho(PHI, RR, flow.M0, flow.gamma);
 
 
 % Initialize error checks
@@ -29,7 +33,10 @@ res = zeros(1,n_t-2);
 
 % Begin iteration
 for n = 3:n_t % loop through time
-    
+%     if (mean2(RHO(:,:,n-1)) ~= 1)
+%        fprintf('Warning: density is not unity!\n'); 
+%     end
+        
     for i = 1:n_T % loop through theta        
         for j = 1:(n_r-1) % loop through radius
             % 5 different control indexes for i and j directions            
@@ -52,74 +59,78 @@ for n = 3:n_t % loop through time
             if j == 1
                 j1 = j+1;
                 j_1 = j1;
-            elseif j == n_r
-                j1 = j+1;
-                j_1 = j-1;
             else
                 j1 = j+1;
                 j_1 = j-1;
             end
             
             % Density averages
-            RHO_i1 = 0.5*(RHO(j0,i1) + RHO(j0,i0));
-            RHO_i0 = 0.5*(RHO(j0,i_1) + RHO(j0,i0));
-            RHO_j1 = 0.5*(RHO(j1, i0) + RHO(j0,i0));
-            RHO_j0 = 0.5*(RHO(j_1, i0) + RHO(j0,i0));
+            RHO_i1 = 0.5*(RHO(j0,i1, n-1) + RHO(j0,i0, n-1));
+            RHO_i0 = 0.5*(RHO(j0,i_1,n-1) + RHO(j0,i0,n-1));
+            RHO_j1 = 0.5*(RHO(j1, i0,n-1) + RHO(j0,i0,n-1));
+            RHO_j0 = 0.5*(RHO(j_1, i0,n-1) + RHO(j0,i0,n-1));
+            
+            if (flow.M0 == 0) && ((RHO_i1 ~= 1) || (RHO_i0 ~= 1) || (RHO_j1 ~= 1) || (RHO_j0 ~= 1) )
+               fprintf('WARNING: DENISTY IS NOT UNITY AT ITER %i, %i, %i\n', n, i, j);  
+            end
             
             % Second Order Derivatives
             PHI_TT = (1/RR(j0,i0)^2)*(1/dT)*(RHO_i1*(PHI(j0,i1,n-1)-PHI(j0,i0,n-1))/dT - RHO_i0*(PHI(j0,i0,n-1)-PHI(j0,i_1,n-1))/dT);
-            PHI_RR = (1/RR(j0,i0))*((0.5*(RR(j1,i0)+RR(j0,i0))*(PHI(j1,i0,n-1)-PHI(j0,i0,n-1))/dr)*RHO_j1 - RHO_j0*(0.5*(RR(j0,i0)+RR(j_1,i0))*(PHI(j0,i0,n-1)-PHI(j_1,i0,n-1))/dr))/dr;
+            PHI_RR = (1/RR(j0,i0))*(RHO_j1*0.5*(RR(j1,i0) + RR(j0,i0))*(PHI(j1,i0,n-1) - PHI(j0,i0,n-1))/dr - RHO_j0*0.5*(RR(j_1,i0) + RR(j0,i0))*(PHI(j0,i0,n-1) - PHI(j_1,i0,n-1))/dr)/dr;
+            
+            if (j == 1) && (PHI(j1,i0) ~= PHI(j_1,i0))
+               fprintf('Neumann Condition not applied for radius!\n'); 
+            end
+            
+            if ((i == 1)||(i == n_T)) && (PHI(j0,i1) ~= PHI(j0,i_1))
+               fprintf('Neumann Condition not applied for THETA!\n'); 
+            end
 
             % Apply Conditions for Time
-            PHI(j0,i0,n) = ((0.5*alpha/dt - 1/dt^2)*PHI(j0,i0,n-2) + 2*PHI(j0,i0,n-1)/dt^2 + PHI_RR + PHI_TT)/(1/dt^2 + 0.5*alpha/dt);
+            PHI(j0,i0,n) = ((0.5*alpha/dt - 1/(dt^2))*PHI(j0,i0,n-2) + 2/(dt^2)*PHI(j0,i0,n-1) + PHI_RR + PHI_TT)/(1/dt^2 + 0.5*alpha/dt);
         end
-        PHI(n_r,i,n) = PHI(n_r,i,n-1); % set the boundary condition
     end
+    PHI(n_r,:,n) = PHI_BC; %PHI(n_r,i,n-1); % set the farfield boundary condition
     
     % Update derivatives
-    [PHI_R(:,:,n), PHI_T(:,:,n), RHO(:,:,n)] = update_derivs(PHI, RR, TT, flow.M0, flow.gamma);
+    [PHI_R(:,:,n), PHI_T(:,:,n), RHO(:,:,n)] = update_rho(PHI, RR, flow.M0, flow.gamma);
     
     % Run Residual Check
     difference = abs(PHI(:,:,n) - PHI(:,:,n-1));
     res(n-2) = max(difference(:));
 end
 
-function [phi_r, phi_th, rho] = update_derivs(phi, rr, tt, M0, gam)
+function [phi_r, phi_th, rho] = update_rho(phi, rr, M0, gam)
     
-if ~exist('n', 'var')
-    n = 2;
-end
+    if ~exist('n', 'var')
+        n = 2;
+    end
 
-% initialize temp vals for PHI_R, PHI_T, and rho
-phi_r = zeros(size(phi(:,:,n)));
-phi_th = zeros(size(phi(:,:,n)));
-rho = zeros(size(phi(:,:,n)));
-% phi_t = zeros(size(rho));
+    % initialize temp vals for PHI_R, PHI_T, and rho
+    phi_r = zeros(size(phi(:,:,n)));
+    phi_th = zeros(size(phi(:,:,n)));
+    rho = ones(size(phi(:,:,n)));
+    % phi_t = zeros(size(rho));
 
     % Radial derivative
     for jj = 2:(n_r-1)
-       phi_r(jj,:) =  (phi(jj+1,:,n)-phi(jj-1,:,n))./(rr(jj+1,:) - rr(jj-1,:));
+       phi_r(jj,:) =  (phi(jj+1,:,n)-phi(jj-1,:,n))./(2*dr); % central finite difference
     end
 
-    phi_r(1,:) = zeros(1,n_T);
-    phi_r(end,:) = (phi(end,:,n)-phi(end-1,:,n))./(rr(end,:) - rr(end-1,:));
+    phi_r(1,:) = zeros(size(phi_r(1,:))); % surface boundary condition
+    phi_r(end,:) = (phi(end,:,n)-phi(end-1,:,n))./(dr); % backwards
 
     % Angular derivative
-    for ii = 1:n_T
+    for ii = 2:(n_T-1)
         ii0 = ii;
-        if ii == 1
-            ii1 = ii+1;
-            ii_1 = n_T;
-        elseif ii == n_T
-            ii1 = 1;
-            ii_1 = ii-1;
-        else
-            ii1 = ii+1;
-            ii_1 = ii-1;
-        end 
-
-        phi_th(:,ii0) =  (1./rr(:,ii0)).*(phi(:,ii1,n)-phi(:,ii_1,n))./(tt(:,ii1) - tt(:,ii_1));        
+        ii1 = ii+1;
+        ii_1 = ii-1;
+        % Central differencing scheme
+        phi_th(:,ii0) =  (1./rr(:,ii0)).*(phi(:,ii1,n)-phi(:,ii_1,n))./dT;        
     end
+
+    phi_th(:,1) = zeros(size(phi_th(:,1))); % symmetric boundary condition
+    phi_th(:,n_T) = zeros(size(phi_th(:,n_T))); % symmetric boundary condition
 
     % Time Derivative
     phi_t = (phi(:,:,n) - phi(:,:,n-1))/dt;
@@ -143,9 +154,15 @@ rho = zeros(size(phi(:,:,n)));
         end
         for jj = 2:n_r % Loop through Radii
             rho(jj,ii0) = rho_bar(jj, ii0) - eps(jj, ii0)*(phi_th(jj, ii0)*(rho_bar(jj,ii0) - rho_bar(jj,ii_1)) + phi_r(jj,ii0)*(rho_bar(jj,ii0) - rho_bar(jj-1, ii0)));
+            if rho(jj,ii0) < 0
+                rho(jj,ii0) = 0;
+            end
         end
 
         rho(1,ii0) = rho_bar(1,ii0) - eps(1,ii0) * (phi_th(1,ii0) * (rho_bar(1,ii0) - rho_bar(1, ii_1))); % all PHI_R's at surface are zero
+        if rho(1,ii0) < 0
+            rho(1,ii0) = 0;
+        end
     end
 end
 
