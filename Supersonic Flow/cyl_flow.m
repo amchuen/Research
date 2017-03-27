cd67ea259e18d95f675c66a95fb78054186ce699
Andrew Chuen <amchuen@ucdavis.edu>
2017-03-26 22:02:52 -0700

    Obtained Shock From Aritficial Viscosity as applied to flow potential

--- Supersonic Flow/cyl_flow.m
+++ Supersonic Flow/cyl_flow.m
@@ -9,9 +9,11 @@ dr = 0.1;
 dT = 0.01*pi;
 dt = 0.1*dr;
 
+alpha = 2.5;
+
 % Cylinder Dimensions
 r_cyl = 1.0;
-r_max = 3;
+r_max = 8;
 
 % Field Axis Values
 r_vals = r_cyl:dr:(r_max+dr);
@@ -37,7 +39,7 @@ run.grid.YY = run.grid.RR .* sin(run.grid.TT);
 % Initialize flow properties
 run.flow.gamma = 1.4;
 run.flow.PHI_INIT = run.grid.XX; 
-M0 = [0.4]; % , 0.3, 0.35
+M0 = [0.55];%, 0.4, 0.55]; % , 0.3, 0.35
 
 %% Loop and Solve for Convergence w/ Time
 
@@ -45,6 +47,7 @@ resCheck = figure();
 title('Residual Check');
 theta = figure();
 % Initialize time grids
+show_field = 1;
 run.times.start = start; % seconds
 for nn = 1:length(M0)
     fprintf('Simulation %i Started...\n', nn);
@@ -53,15 +56,17 @@ for nn = 1:length(M0)
     end
     
     run(nn).flow.M0 = M0(nn);
-    if nn > 1
-       run(nn).flow.PHI_INIT = run(nn-1).sol.PHI(:,:,end); 
-    end
+    run(nn).flow.visc = 1;
+%     if nn > 1
+%        run(nn).flow.PHI_INIT = run(nn-1).sol.PHI(:,:,end); 
+%     end
     run(nn).times.dt = dt;
-    run(nn).times.alpha = 0.0;%5*1.5;%10/dt(nn);
-    run(nn).times.stop = 202 * dt;
+    run(nn).times.alpha = alpha;%10/dt(nn);
+    run(nn).times.stop = 302 * dt;
 
     % Run Solver
-    [run(nn).sol.PHI, run(nn).sol.PHI_R, run(nn).sol.PHI_T, run(nn).sol.RHO, run(nn).sol.res, ind, x_res, y_res] = flowSolve_comp(run(nn).grid, run(nn).flow, run(nn).times);
+%     dbstop in flowSolve_comp if ~isreal([PHI_TT, PHI_RR, RHO_TT, RHO_RR])
+    [run(nn).sol.PHI, run(nn).sol.PHI_R, run(nn).sol.PHI_T, run(nn).sol.RHO,run(nn).sol.M_IJ, run(nn).sol.res, ind, x_res, y_res] = flowSolve_comp(run(nn).grid, run(nn).flow, run(nn).times);
     fprintf('Residual: %0.5f\n', run(nn).sol.res(end));
     fprintf('Simulation %i Complete...\n', nn);
     
@@ -71,33 +76,42 @@ for nn = 1:length(M0)
     plot((0:1:((run(nn).times.stop - run(nn).times.start)/run(nn).times.dt - 3)), log10(run(nn).sol.res));
     hold off;
     
-    % Plot Fields
-    PHI_X = run(nn).sol.PHI_R(:,:,end).*cos(run(nn).grid.TT) - run(nn).sol.PHI_T(:,:,end).*sin(run(nn).grid.TT);
-    PHI_Y = run(nn).sol.PHI_R(:,:,end).*sin(run(nn).grid.TT) + run(nn).sol.PHI_T(:,:,end).*cos(run(nn).grid.TT);
-%     figure(); % cp plots
-%     contourf(run(nn).grid.XX, run(nn).grid.YY, 1-(PHI_X.^2 + PHI_Y.^2), 50); %./((RR.*cos(TT)).^2)
-%     title(sprintf('Pressure Coefficient Contours (Sim %i)', nn));
-%     colorbar('eastoutside');
-    
-%     figure(); % field potential
-%     contourf(run(nn).grid.XX, run(nn).grid.YY, run(nn).sol.PHI(:,:,end), 50);
-%     title(sprintf('Field Potential (Sim %i)', nn));
-%     colorbar('eastoutside');
-%     axis equal
-    
-%     figure(); % x-dir velocity plots
-%     contourf(run(nn).grid.XX, run(nn).grid.YY, PHI_X, 50); %./((RR.*cos(TT)).^2)
-%     title(sprintf('U velocity (Sim %i)', nn));
-%     colorbar('eastoutside');
+    if show_field == 1
+        % Plot Fields
+        PHI_X = run(nn).sol.PHI_R(:,:,end).*cos(run(nn).grid.TT) - run(nn).sol.PHI_T(:,:,end).*sin(run(nn).grid.TT);
+        PHI_Y = run(nn).sol.PHI_R(:,:,end).*sin(run(nn).grid.TT) + run(nn).sol.PHI_T(:,:,end).*cos(run(nn).grid.TT);
+        figure(); % cp plots
+        contourf(run(nn).grid.XX, run(nn).grid.YY, 1-(PHI_X.^2 + PHI_Y.^2), 50); %./((RR.*cos(TT)).^2)
+        title(sprintf('Pressure Coefficient Contours (Sim %i)', nn));
+        colorbar('eastoutside');
+
+        figure(); % field potential
+        contourf(run(nn).grid.XX, run(nn).grid.YY, run(nn).sol.PHI(:,:,end), 50);
+        title(sprintf('Field Potential (Sim %i)', nn));
+        colorbar('eastoutside');
+        axis equal
+
+        figure(); % x-dir velocity plots
+        contourf(run(nn).grid.XX, run(nn).grid.YY, run(nn).sol.PHI_T(:,:,end), 50); %./((RR.*cos(TT)).^2)
+        title(sprintf('PHI_\theta velocity (Sim %i)', nn));
+        colorbar('eastoutside');
+        
+        figure();
+        contourf(run(nn).grid.XX, run(nn).grid.YY, run(nn).sol.M_IJ(:,:,end), 150);
+        title(sprintf('Mach Number (Sim %i)', nn));
+        colorbar('eastoutside');
+    end
     figure(theta);
     hold on;
-    plot(run(nn).grid.TT(1,:), run(nn).sol.PHI_T(1,:,end));
+    plot(run(nn).grid.TT(1,:)*180/pi, run(nn).sol.PHI_T(1,:,end));
+    xlabel('\theta');
+    ylabel('\phi_{\theta}');
     hold off;
     
     figure();
     hold on;
     for i = 1:length(x_res)
-        plot(run.grid.XX(y_res(i), x_res(i)), run.grid.YY(y_res(i), x_res(i)), 'o');
+        plot(run(nn).grid.XX(y_res(i), x_res(i)), run(nn).grid.YY(y_res(i), x_res(i)), 'o');
         hold on;
     end
     axis equal;
@@ -109,15 +123,22 @@ end
 
 %% Display Results
 
-% figure();
-% contourf(grid.XX, grid.YY, 1-((PHI_R(:,:,end-1).^2 + PHI_T(:,:,end-1).^2)), 50); %./((RR.*cos(TT)).^2)
+% PHI_X = run(nn).sol.PHI_R(:,:,end).*cos(run(nn).grid.TT) - run(nn).sol.PHI_T(:,:,end).*sin(run(nn).grid.TT);
+% PHI_Y = run(nn).sol.PHI_R(:,:,end).*sin(run(nn).grid.TT) + run(nn).sol.PHI_T(:,:,end).*cos(run(nn).grid.TT);
+% figure(); % cp plots
+% contourf(run(nn).grid.XX, run(nn).grid.YY, 1-(PHI_X.^2 + PHI_Y.^2), 50); %./((RR.*cos(TT)).^2)
+% title(sprintf('Pressure Coefficient Contours (Sim %i)', nn));
 % colorbar('eastoutside');
 % 
-% figure();
-% contourf(grid.XX, grid.YY, PHI_R(:,:,end-1).*cos(grid.TT) - PHI_T(:,:,end-1).*sin(grid.TT), 50); %./((RR.*cos(TT)).^2)
+% figure(); % field potential
+% contourf(run(nn).grid.XX, run(nn).grid.YY, run(nn).sol.PHI(:,:,end), 50);
+% title(sprintf('Field Potential (Sim %i)', nn));
 % colorbar('eastoutside');
+% axis equal
 % 
-% figure();
-% quiver(grid.XX, grid.YY, PHI_R(:,:,end-1).*cos(grid.TT) - PHI_T(:,:,end-1).*sin(grid.TT), PHI_R(:,:,end-1).*sin(grid.TT) + PHI_T(:,:,end-1).*cos(grid.TT));
+% figure(); % x-dir velocity plots
+% contourf(run(nn).grid.XX, run(nn).grid.YY, PHI_X, 50); %./((RR.*cos(TT)).^2)
+% title(sprintf('U velocity (Sim %i)', nn));
+% colorbar('eastoutside');
 
 %% Check for Convergence
--- Supersonic Flow/flowSolve_comp.m
+++ Supersonic Flow/flowSolve_comp.m
@@ -1,4 +1,4 @@
-function [PHI, PHI_R, PHI_T, RHO, res, ind, x_res, y_res] = flowSolve_comp(grid, flow, times)
+function [PHI, PHI_R, PHI_T, RHO, M_IJ, res, ind, x_res, y_res] = flowSolve_comp(grid, flow, times)
 
 % Initilize Grid
 dr = grid.dr;
@@ -25,8 +25,9 @@ PHI_BC = RR(n_r,:).*cos(TT(n_r,:)) + cos(TT(n_r,:))./RR(n_r,:);
 RHO = zeros(size(PHI));
 PHI_R = zeros(size(PHI));
 PHI_T = zeros(size(PHI));
+M_IJ = zeros(size(PHI));
 [PHI_R(:,:,2), PHI_T(:,:,2), RHO(:,:,2)] = update_rho(PHI, RR, flow.M0, flow.gamma);
-e_visc = -0.001;
+% e_visc = -0.001;
 
 
 % Initialize error checks
@@ -83,8 +84,15 @@ for n = 3:n_t % loop through time
             PHI_RR = (1/RR(j0,i0))*(RHO_j1*0.5*(RR(j1,i0) + RR(j0,i0))*(PHI(j1,i0,n-1) - PHI(j0,i0,n-1))/dr - RHO_j0*0.5*(RR(j_1,i0) + RR(j0,i0))*(PHI(j0,i0,n-1) - PHI(j_1,i0,n-1))/dr)/dr;
             
             % Divergence of Density
-            RHO_TT = (1/RR(j0,i0)^2)*(1/dT^2)*(RHO(j0,i1,n-1) - 2*RHO(j0,i0,n-1)+RHO(j0,i_1,n-1));
-            RHO_RR = (1/RR(j0,i0))*(0.5*(RR(j1,i0) + RR(j0,i0))*(RHO(j1,i0,n-1) - RHO(j0,i0,n-1))/dr - 0.5*(RR(j_1,i0) + RR(j0,i0))*(RHO(j0,i0,n-1) - RHO(j_1,i0,n-1))/dr)/dr;
+%             RHO_TT = (1/RR(j0,i0)^2)*(1/dT^2)*(RHO(j0,i1,n-1) - 2*RHO(j0,i0,n-1)+RHO(j0,i_1,n-1));
+%             RHO_RR = (1/RR(j0,i0))*(0.5*(RR(j1,i0) + RR(j0,i0))*(RHO(j1,i0,n-1) - RHO(j0,i0,n-1))/dr - 0.5*(RR(j_1,i0) + RR(j0,i0))*(RHO(j0,i0,n-1) - RHO(j_1,i0,n-1))/dr)/dr;
+
+            % Local viscosity in PHI at top of cylinder
+%             if (TT(j0, i0)*180/pi >= 45) || (TT(j0, i0)*180/pi <= 135)
+%                 e_visc = 0.5*1.25e-7;
+%             else
+%                 e_visc = 0.0;
+%             end
             
             if (j == 1) && (PHI(j1,i0) ~= PHI(j_1,i0))
                fprintf('Neumann Condition not applied for radius!\n'); 
@@ -93,15 +101,41 @@ for n = 3:n_t % loop through time
             if ((i == 1)||(i == n_T)) && (PHI(j0,i1) ~= PHI(j0,i_1))
                fprintf('Neumann Condition not applied for THETA!\n'); 
             end
+            
+            if ~isreal([PHI_TT, PHI_RR])%, RHO_TT, RHO_RR])
+                fprintf('Complex Result! Please check results and mesh!\n');
+%                 dbstop in flowSolve_comp
+            end
 
             % Apply Conditions for Time ...add viscosity term from divergence of density
-            PHI(j0,i0,n) = ((0.5*alpha/dt - 1/(dt^2))*PHI(j0,i0,n-2) + 2/(dt^2)*PHI(j0,i0,n-1) + PHI_RR + PHI_TT + e_visc.*(RHO_RR + RHO_TT))/(1/dt^2 + 0.5*alpha/dt);
+%             PHI(j0,i0,n) = ((0.5*alpha/dt - 1/(dt^2))*PHI(j0,i0,n-2) + 2/(dt^2)*PHI(j0,i0,n-1) + PHI_RR + PHI_TT)/(1/dt^2 + 0.5*alpha/dt);
+            if (TT(j0, i0)*180/pi >= 45) && (TT(j0, i0)*180/pi <= 135)
+                
+                if flow.visc == 1
+    %                 PHI(j0,i0,n) = ((0.5*alpha/dt - 1/(dt^2))*PHI(j0,i0,n-2) + 2/(dt^2)*PHI(j0,i0,n-1) + PHI_RR + PHI_TT - e_visc.*((PHI(j0, i_1, n-1) - PHI(j0,i0,n-1)) - (PHI(j0,i0,n-1) - PHI(j0,i1, n-1)))/dT)/(1/dt^2 + 0.5*alpha/dt);
+                    e_visc = -1.25e-2;
+                    VISC = - e_visc.*((PHI(j0, i_1, n-1) - PHI(j0,i0,n-1)) - (PHI(j0,i0,n-1) - PHI(j0,i1, n-1)))/dT;
+                elseif flow.visc == 2
+    %                 PHI(j0,i0,n) = ((0.5*alpha/dt - 1/(dt^2))*PHI(j0,i0,n-2) + 2/(dt^2)*PHI(j0,i0,n-1) + PHI_RR + PHI_TT - e_visc.*(2*(PHI(j0, i_1, n-1) - PHI(j0,i0,n-1)) - (PHI(j0,i0,n-1) - 2*PHI(j0,i1, n-1)+PHI(j0,i1+1,n-1)))/dT)/(1/dt^2 + 0.5*alpha/dt);
+                    e_visc = +1.25e-9;
+                    VISC = - e_visc.*((PHI(j0, i_1, n-1) - 2*PHI(j0,i0,n-1)+PHI(j0,i1,n-1)) - (PHI(j0,i0,n-1) - 2*PHI(j0,i1, n-1)+PHI(j0,i1+1,n-1)))/dT;
+%                     disp(i1+1);
+                elseif flow.visc == 3
+    %                 PHI(j0,i0,n) = ((0.5*alpha/dt - 1/(dt^2))*PHI(j0,i0,n-2) + 2/(dt^2)*PHI(j0,i0,n-1) + PHI_RR + PHI_TT - e_visc.*(2*(PHI(j0, i_1, n-1) - PHI(j0,i0,n-1)) - (PHI(j0,i0,n-1) - 2*PHI(j0,i1, n-1)+PHI(j0,i1+1,n-1)))/dT)/(1/dt^2 + 0.5*alpha/dt);
+%                     e_visc = 1.25e-3;
+                    VISC = - e_visc.*((PHI(j0, i_1, n-1) - 2*PHI(j0,i0,n-1)+PHI(j0,i1,n-1)) - (PHI(j0,i0,n-1) - 2*PHI(j0,i_1, n-1)+PHI(j0,i_1-1,n-1)))/dT;
+%                     disp(i1+1);
+                end
+            else
+                VISC = 0;
+            end
+            PHI(j0,i0,n) = ((0.5*alpha/dt - 1/(dt^2))*PHI(j0,i0,n-2) + 2/(dt^2)*PHI(j0,i0,n-1) + PHI_RR + PHI_TT + VISC)/(1/dt^2 + 0.5*alpha/dt);            
         end
     end
     PHI(n_r,:,n) = PHI_BC; %PHI(n_r,i,n-1); % set the farfield boundary condition
     
     % Update derivatives
-    [PHI_R(:,:,n), PHI_T(:,:,n), RHO(:,:,n)] = update_rho(PHI, RR, flow.M0, flow.gamma);
+    [PHI_R(:,:,n), PHI_T(:,:,n), RHO(:,:,n), M_IJ(:,:,n)] = update_rho(PHI, RR, flow.M0, flow.gamma);
     
     % Run Residual Check
     difference = abs(PHI(:,:,n) - PHI(:,:,n-1));
@@ -111,7 +145,7 @@ for n = 3:n_t % loop through time
     x_res(n-2) = xx;
 end
 
-function [phi_r, phi_th, rho] = update_rho(phi, rr, M0, gam)
+function [phi_r, phi_th, rho, M_ij] = update_rho(phi, rr, M0, gam)
     
     if ~exist('n', 'var')
         n = 2;
@@ -126,6 +160,10 @@ function [phi_r, phi_th, rho] = update_rho(phi, rr, M0, gam)
     % Radial derivative
     for jj = 2:(n_r-1)
        phi_r(jj,:) =  (phi(jj+1,:,n)-phi(jj-1,:,n))./(2*dr); % central finite difference
+       
+       if ~isreal(phi_r(jj,:))
+            fprintf('Imaginary value detected for Phi_r!\n');
+       end
     end
 
     phi_r(1,:) = zeros(size(phi_r(1,:))); % surface boundary condition
@@ -137,7 +175,12 @@ function [phi_r, phi_th, rho] = update_rho(phi, rr, M0, gam)
         ii1 = ii+1;
         ii_1 = ii-1;
         % Central differencing scheme
-        phi_th(:,ii0) =  (1./rr(:,ii0)).*(phi(:,ii1,n)-phi(:,ii_1,n))./dT;        
+%         phi_th(:,ii0) =  (1./rr(:,ii0)).*(phi(:,ii1,n)-phi(:,ii_1,n))./(2*dT);
+        phi_th(:,ii0) = (phi(:,ii1,n)-phi(:,ii_1,n))./(2*dT);
+        
+        if ~isreal(phi_th(:,ii0))
+            fprintf('Imaginary value detected for Phi_th!\n');
+       end
     end
 
     phi_th(:,1) = zeros(size(phi_th(:,1))); % symmetric boundary condition
@@ -147,33 +190,54 @@ function [phi_r, phi_th, rho] = update_rho(phi, rr, M0, gam)
     phi_t = (phi(:,:,n) - phi(:,:,n-1))/dt;
 
     % Local speed of sound
-    a2_ij = (2/(M0^2) - (gam - 1).*(phi_r.^2 + (phi_th./rr).^2 - 1));
+%     a2_ij = (2/(M0^2) - (gam - 1).*(phi_r.^2 + (phi_th./rr).^2 - 1));
+    a2_ij = (gam - 1).*(0.5 + 1./((gam-1)*M0^2) - 0.5.*(phi_r.^2 + (phi_th./rr).^2));
 
     % Local Mach number & artificial viscosity
-    M_ij = (((phi_th./rr).^2 + phi_r.^2)./a2_ij).^2;
-    eps = max(zeros(size(M_ij)), 1 - 0.9./(M_ij.^2));
+    M2_ij = (((phi_th./rr).^2 + phi_r.^2)./a2_ij);
+    M_ij = M2_ij.^2;
+%     eps = max(zeros(size(M2_ij)), 1 - 0.9./(M2_ij));
+    eps = zeros(size(M2_ij));
+%     eps = 0.02*ones(size(M_ij));%max(zeros(size(M_ij)), 1 - 0.9./(M0^2 .* ones(size(M_ij))));
 
     % Local density calculations
+%     rho_bar = (1 + 0.5.*(gam-1).*M0^2.*(phi_r.^2 + (phi_th./rr).^2 + 2*phi_t)).^(-1/(gam-1));
     rho_bar = (1 - 0.5.*(gam-1).*M0^2.*(phi_r.^2 + (phi_th./rr).^2 + 2*phi_t - 1)).^(1/(gam-1));
+%     rho_bar(1,:) = ones(size(rho_bar(1,:)));
 
     for ii = 1:n_T % loop through Thetas
         ii0 = ii;
         if ii == 1
             ii_1 = n_T;
+            ii1 = ii + 1;
+        elseif ii == n_T
+            ii_1 = ii - 1;
+            ii1 = 1;
         else
-            ii_1 = ii-1;
+            ii_1 = ii0-1;
+            ii1 = ii0 + 1;
         end
         for jj = 2:(n_r-1) % Loop through Radii
-            rho(jj,ii0) = rho_bar(jj, ii0) - eps(jj, ii0)*(phi_th(jj, ii0)*(rho_bar(jj,ii0) - rho_bar(jj,ii_1)) + phi_r(jj,ii0)*(rho_bar(jj,ii0) - rho_bar(jj-1, ii0)));
-            if rho(jj,ii0) < 0
-                rho(jj,ii0) = 0;
+%             rho(jj,ii0) = rho_bar(jj, ii0) - eps(jj, ii0)*(phi_th(jj, ii0)*(rho_bar(jj,ii0) - rho_bar(jj,ii_1)) + phi_r(jj,ii0)*(rho_bar(jj,ii0) - rho_bar(jj-1, ii0)));
+            rho(jj, ii0) = rho_bar(jj, ii0) - eps(jj, ii0)*(phi_th(jj,ii0)/rr(jj,ii0) * (rho_bar(jj, ii0) - rho_bar(jj, ii1))/dT + phi_r(jj, ii0) * (rho_bar(jj+1, ii0) - rho_bar(jj, ii0))/dr);
+            if ~isreal(rho(jj, ii0))
+                fprintf('Imaginary value detected for density!\n');
             end
+            
+%             if (rho(jj,ii0) < 0) || (~isreal(rho(jj,ii0)))
+%                 rho(jj,ii0) = 0;
+%             end
         end
 
-        rho(1,ii0) = rho_bar(1,ii0) - eps(1,ii0) * (phi_th(1,ii0) * (rho_bar(1,ii0) - rho_bar(1, ii_1))); % all PHI_R's at surface are zero
-        if rho(1,ii0) < 0
-            rho(1,ii0) = 0;
+%         rho(1,ii0) = rho_bar(1,ii0) - eps(1,ii0) * (phi_th(1,ii0) * (rho_bar(1,ii0) - rho_bar(1, ii_1))); % all PHI_R's at surface are zero
+        rho(1, ii0) = rho_bar(1, ii0) - eps(1, ii0)*(phi_th(1,ii0)/rr(1,ii0) * (rho_bar(1, ii_1) - rho_bar(1, ii0))/dT);
+        if ~isreal(rho(1, ii0))
+            fprintf('Imaginary value detected for density!\n');
         end
+        
+%         if (rho(1,ii0) < 0) || (~isreal(rho(1,ii0)))
+%             rho(1,ii0) = 0;
+%         end
         
         rho(n_r, ii0) = 1;
     end
