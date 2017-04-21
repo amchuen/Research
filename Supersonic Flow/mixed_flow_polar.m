02bafc1f9fdfc1edfa06236c1d30bccfddf1fe44
Andrew Chuen <amchuen@ucdavis.edu>
2017-04-21 15:18:33 -0700

    Achived shock w/o noise for cylinder

--- Supersonic Flow/mixed_flow_polar.m
+++ Supersonic Flow/mixed_flow_polar.m
@@ -9,11 +9,11 @@ dr = 0.1;
 dT = 0.005*pi;
 dt = 0.01;
 
-alpha = 25.0;
+alpha = 30.0;
 
 % Cylinder Dimensions
 r_cyl = 1.0;
-r_max = 10;
+r_max = 8.0;
 
 % Field Axis Values
 r_vals = (r_cyl+0.5*dr):dr:(r_max);
@@ -31,7 +31,7 @@ YY = RR .* sin(TT);
 
 % Fluid Params
 gam = 1.4; % heat 
-M0 = 0.49;
+M0 = 0.52;
 visc = 0.0;
 
 %% FIELD VARIABLE INITIALIZATION
@@ -43,7 +43,7 @@ PHI(:,:,2) = PHI(:,:,1);
 res = 1;
 ind = 0;
 iter = 1.0;
-tol = 1e-3;
+tol = 1e-5;
 
 BC.dirichlet.Vr_II = cos(TT(end,:)).*(1 - (r_cyl^2)./(RR(end,:).^2));
 BC.dirichlet.PHI_II = (RR(end,:) + (r_cyl^2)./(RR(end,:))).*cos(TT(end,:));
@@ -65,6 +65,23 @@ while res(end) > tol % iterate through time
     if ~isreal(PHI(:,:,iter)) || any(any(isnan(PHI(:,:,iter))))
         fprintf('PHI exhibits non-solutions (either non-real or NaN) in nodes!\n');
     end
+    
+    % Get artificialvisocisty at the nodes
+    U_n = zeros(size(RR)); % PHI_X
+    V_n = zeros(size(RR)); % PHI_Y
+    for i = 2:(size(PHI,2)-1) % loop through thetas
+        U_n(:,i) = (PHI(:,i+1,iter) - PHI(:,i-1,iter))./(2*dT .* RR(:, i));
+    end
+    
+    V_n(end,:) = BC.dirichlet.Vr_II; % apply far-field potential flow condition
+    for ii = 2:(size(PHI,1)-1) % loop through radii
+        V_n(ii,:) = (PHI(ii+1,:,iter) - PHI(ii-1,:,iter))./(2*dr); % central difference
+    end
+    
+    q2_ij = U_n.^2 + V_n.^2;
+    a2_ij = (1./M0.^2)-0.5*(gam-1).*(q2_ij - 1);
+    eps_ij = max(zeros(size(a2_ij)), 1 - 0.99.*(a2_ij./q2_ij));
+%     M2_ij = q2_ij./a2_ij; % local Mach number
 
     % Initialize Density
     phi_t = (PHI(:,:,iter) - PHI(:,:,iter-1))./dt;
@@ -85,9 +102,17 @@ while res(end) > tol % iterate through time
             % same if reflecte across boundary
             
             % Calculate average velocities...
-            u_avg(j,ii) = 0.5*((PHI(j,ii+1,iter) - PHI(j,ii,iter))/(RR(j,ii)*dT) + (PHI(j-1,ii+1,iter) - PHI(j-1,ii,iter))/(RR(j-1,ii)*dT));
-            v_avg(j,ii) = 0.5*((PHI(j,ii,iter) - PHI(j-1,ii,iter))/dr + (PHI(j,ii+1,iter) - PHI(j-1,ii+1,iter))/dr);
-            phi_t_avg(j,ii) = mean([phi_t(j, ii), phi_t(j-1, ii), phi_t(j-1, ii+1), phi_t(j, ii+1)]);            
+            if j ==1 % Neumann condition
+                u_avg(j,ii) = (PHI(j,ii+1,iter) - PHI(j,ii,iter))/(RR(j,ii)*dT);
+                v_avg(j,ii) = 0.0;
+                phi_t_avg(j,ii) = mean([phi_t(j, ii), phi_t(j, ii+1)]);
+            else
+                u_avg(j,ii) = 0.5*((PHI(j,ii+1,iter) - PHI(j,ii,iter))/(RR(j,ii)*dT) + (PHI(j-1,ii+1,iter) - PHI(j-1,ii,iter))/(RR(j-1,ii)*dT));
+                v_avg(j,ii) = 0.5*((PHI(j,ii,iter) - PHI(j-1,ii,iter))/dr + (PHI(j,ii+1,iter) - PHI(j-1,ii+1,iter))/dr);
+                phi_t_avg(j,ii) = mean([phi_t(j, ii), phi_t(j-1, ii), phi_t(j-1, ii+1), phi_t(j, ii+1)]);
+            end
+            
+                        
         end
     end
     
@@ -99,7 +124,7 @@ while res(end) > tol % iterate through time
     % Calculate Local (Artificial) Viscosity
     q2_avg = u_avg.^2 + v_avg.^2; % total velocity
     a2_avg = (1./M0.^2)-0.5*(gam-1).*(q2_avg - 1);
-    eps_ij = 1.2.*max(zeros(size(rho_ij)), 1 - 0.99.*(a2_avg./q2_avg));
+%     eps_ij = 1.2.*max(zeros(size(rho_ij)), 1 - 0.99.*(a2_avg./q2_avg));
     if any(any(eps_ij ~= 0)) && (visc_on ==0) % viscosity not being used and then turned on
         visc_on = 1;
         fprintf('Viscosity model activated! Iteration: %i\n', iter);        
@@ -114,20 +139,17 @@ while res(end) > tol % iterate through time
         fprintf('Density is complex! Please resolve before continuing!\n');
     end
     
-    % Calculate viscosity corrections
+    % Calculate viscosity dissipation
     del_rho_dT = zeros(size(rho_ij));
     for i = 1:(size(rho_ij,2)) % loop through theta
         if i ==1 % Neumann BC
-            i1 = i+1;
-            i_1 = i;
+            del_rho_dT(:,i) = 0.5.*(rho_ij(:,i+1)-rho_ij(:,i))-0.5.*(sign(u_avg(:,i))).*(rho_ij(:,i+1)-rho_ij(:,i));
         elseif i == size(rho_ij,2) % Neumann BC
-            i1 = i;
-            i_1 = i-1;
+            del_rho_dT(:,i) = 0.5.*(rho_ij(:,i)-rho_ij(:,i-1))-0.5.*(sign(u_avg(:,i))).*(-rho_ij(:,i)+rho_ij(:,i-1));
         else
-            i1 = i+1;
-            i_1 = i-1;
+            del_rho_dT(:,i) = 0.5.*(rho_ij(:,i+1)-rho_ij(:,i-1))-0.5.*(sign(u_avg(:,i))).*(rho_ij(:,i+1)-2.*rho_ij(:,i)+rho_ij(:,i-1));
         end
-        del_rho_dT(:,i) = 0.5.*(rho_ij(:,i1)-rho_ij(:,i_1))-0.5.*(sign(u_avg(:,i))).*(rho_ij(:,i1)-2.*rho_ij(:,i)+rho_ij(:,i_1));
+        
     end
     
     del_rho_dr = zeros(size(rho_ij));
@@ -135,44 +157,65 @@ while res(end) > tol % iterate through time
         if ii ==1 % Neumann BC
             del_rho_dr(ii,:) = 0.5*(rho_ij(ii+1,:) - rho_ij(ii,:))-0.5.*(sign(v_avg(ii,:))).*(rho_ij(ii+1,:)-rho_ij(ii,:));
         elseif ii == size(rho_ij,1) % Dirichlet BC
-            del_rho_dr(ii,:) = 0.5*(ones(size(rho_ij(ii,:))) - rho_ij(ii,:))-0.5.*(sign(v_avg(ii,:))).*(ones(size(rho_ij(ii,:)))-2.*rho_ij(ii,:)-rho_ij(ii-1,:)); % assumes density to be zero at far-field
+            del_rho_dr(ii,:) = 0.5*(ones(size(rho_ij(ii,:))) - rho_ij(ii-1,:))-0.5.*(sign(v_avg(ii,:))).*(ones(size(rho_ij(ii,:)))-2.*rho_ij(ii,:)-rho_ij(ii-1,:)); % assumes density to be zero at far-field
         else
-            del_rho_dr(ii,:) = 0.5*(rho_ij(ii+1,:) - rho_ij(ii,:))-0.5.*(sign(v_avg(ii,:))).*(rho_ij(ii+1,:)-2.*rho_ij(ii,:)-rho_ij(ii-1,:));
+            del_rho_dr(ii,:) = 0.5*(rho_ij(ii+1,:) - rho_ij(ii-1,:))-0.5.*(sign(v_avg(ii,:))).*(rho_ij(ii+1,:)-2.*rho_ij(ii,:)-rho_ij(ii-1,:));
         end
     end
     
     rho_ds = (u_avg./sqrt(q2_avg)).*del_rho_dT + (v_avg./sqrt(q2_avg)).*del_rho_dr;
-    RHO = rho_ij - eps_ij.*rho_ds;
+%     rho_ij = rho_ij;% - eps_ij.*rho_ds;
     
-    if (M0 == 0) && any(any(RHO~=1))
+    if (M0 == 0) && any(any(rho_ij~=1))
         fprintf('Incompressible Case not satisfied!\n');
     end
     
     % Calculate Average Densities on Grid
-    RHO_Tavg = zeros(size(RHO));
-    for i = 1:size(RHO,1) % loop through radius
-        if i == size(RHO,1)
-            RHO_Tavg(i,:) = 0.5.*(RHO(i,:) + ones(size(RHO(i,:)))); % assume density is 1 at farfield
+    RHO_Tavg = zeros(size(rho_ij)); % in theta direction
+    for j = 1:size(rho_ij,1) % loop through radius
+        if j == size(rho_ij,1)
+            RHO_Tavg(j,:) = 0.5.*(rho_ij(j,:) + ones(size(rho_ij(j,:)))); % assume density is 1 at farfield
         else
-            RHO_Tavg(i,:) = 0.5.*(RHO(i,:) + RHO(i+1,:));
+            RHO_Tavg(j,:) = 0.5.*(rho_ij(j,:) + rho_ij(j+1,:));
         end
     end
     
     RHO_Ravg = zeros(size(RR));
     for i = 1:size(RHO_Ravg,2) % loop through Thetas in original grid
         if i == 1 % Apply Neumann condition
-           i1 = i;
-           i_1 = i1;
+            RHO_Ravg(:,i) = 0.5.*(rho_ij(:,i) + rho_ij(:,i)); 
         elseif i == size(RHO_Ravg,2) % Apply Neumann Condition
-            i1 = i-1;
-            i_1 = i1;
+            RHO_Ravg(:,i) = 0.5.*(rho_ij(:,i-1) + rho_ij(:,i-1)); 
         else
-            i1 = i;
-            i_1 = i-1;
+            RHO_Ravg(:,i) = 0.5.*(rho_ij(:,i) + rho_ij(:,i-1)); 
         end
-        RHO_Ravg(:,i) = 0.5.*(RHO(:,i1) + RHO(:,i_1)); 
+        
     end
     
+    % Calculate Averaged Dissipation on Grid
+    RHO_dsT = zeros(size(rho_ds)); % in theta direction
+    for j = 1:size(rho_ds,1) % loop through radius
+        if j == size(rho_ds,1)
+            RHO_dsT(j,:) = 0.5.*(rho_ds(j,:) + ones(size(rho_ds(j,:)))); % assume density is 1 at farfield
+        else
+            RHO_dsT(j,:) = 0.5.*(rho_ds(j,:) + rho_ds(j+1,:));
+        end
+    end
+    
+    RHO_dsR = zeros(size(RR)); % in radial direction
+    for i = 1:size(RHO_dsR,2) % loop through Thetas in original grid
+        if i == 1 % Apply Neumann condition
+            RHO_dsR(:,i) = 0.5.*(rho_ds(:,i) + rho_ds(:,i)); 
+        elseif i == size(RHO_dsR,2) % Apply Neumann Condition
+            RHO_dsR(:,i) = 0.5.*(rho_ds(:,i-1) + rho_ds(:,i-1)); 
+        else
+            RHO_dsR(:,i) = 0.5.*(rho_ds(:,i) + rho_ds(:,i-1)); 
+        end
+        
+    end
+    
+    
+    % Elliptic Solver    
     for i = 1:length(T_vals) % loop through theta        
         for j = 1:(length(r_vals)-1) % loop through radius
             % 5 different control indexes for i and j directions            
@@ -215,13 +258,20 @@ while res(end) > tol % iterate through time
             % Radius Averages
             RR_j1 = 0.5*(RR(j1, i0) + RR(j0,i0));
             
+            % Density Averages  -> USE VICOSITY AT NODE IJ INSTEAD OF
+            % IN-BETWEEN GRID! (Jameson)
+            RHO_I1J = RHO_Tavg(j0,i1r) - eps_ij(j0,i0)*RHO_dsT(j0,i1r);
+            RHO_I_1J = RHO_Tavg(j0,i_1r) - eps_ij(j0,i0)*RHO_dsT(j0,i_1r);
+            RHO_IJ1 = RHO_Ravg(j1,i0) - eps_ij(j0,i0)*RHO_dsR(j1,i0);
+            RHO_IJ_1 = RHO_Ravg(j0,i0) - eps_ij(j0,i0)*RHO_dsR(j0,i0);
+            
             if j == 1
-                PHI_RR = (RR_j1*RHO_Ravg(j1,i0)*(PHI(j1,i0,iter) - PHI(j0,i0,iter))/dr)/(RR(j0,i0)*dr);
+                PHI_RR = (RR_j1*RHO_IJ1*(PHI(j1,i0,iter) - PHI(j0,i0,iter))/dr)/(RR(j0,i0)*dr);
             else
-                PHI_RR = (RR_j1*RHO_Ravg(j1,i0)*(PHI(j1,i0,iter) - PHI(j0,i0,iter))/dr - RR_j0*RHO_Ravg(j0,i0)*(PHI(j0,i0,iter) - PHI(j_1,i0,iter))/dr)/(RR(j0,i0)*dr);
+                PHI_RR = (RR_j1*RHO_IJ1*(PHI(j1,i0,iter) - PHI(j0,i0,iter))/dr - RR_j0*RHO_IJ_1*(PHI(j0,i0,iter) - PHI(j_1,i0,iter))/dr)/(RR(j0,i0)*dr);
             end
             
-            PHI_TT = (1/RR(j0,i0)^2)*(1/dT)*(RHO_Tavg(j0, i1r)*(PHI(j0,i1,iter) - PHI(j0,i0,iter))/dT - RHO_Tavg(j0, i_1r)*(PHI(j0,i0,iter) - PHI(j0,i_1,iter))/dT);
+            PHI_TT = (1/RR(j0,i0)^2)*(1/dT)*(RHO_I1J*(PHI(j0,i1,iter) - PHI(j0,i0,iter))/dT - RHO_I_1J*(PHI(j0,i0,iter) - PHI(j0,i_1,iter))/dT);
             
 %             PHI(j0,i0,iter+1) = ((0.5*alpha/dt - 1/(dt^2))*PHI(j0,i0,iter-1) + 2/(dt^2)*PHI(j0,i0,iter) + PHI_RR + PHI_TT)/(1/dt^2 + 0.5*alpha/dt);
             PHI(j0,i0,iter+1) = (PHI_RR + PHI_TT + 2/(dt^2)*PHI(j0,i0,iter) - (1/dt^2 - 0.5*alpha/dt)*PHI(j0,i0,iter-1))/(1/dt^2 + 0.5*alpha/dt);
@@ -235,21 +285,6 @@ while res(end) > tol % iterate through time
     difference = abs(PHI(:,:,iter+1) - PHI(:,:,iter));
     [res(iter), ind(iter)] = max(difference(:));
     
-%     % Calculate Velocity as function of PHI(n)
-%     U_n = zeros(size(RR)); % PHI_X
-%     V_n = zeros(size(RR)); % PHI_Y
-%     for i = 2:(size(PHI,2)-1) % loop through thetas
-%         U_n(:,i) = (PHI(:,i+1,iter) - PHI(:,i-1,iter))./(2*dT .* RR(:, i));
-%     end
-%     
-%     V_n(end,:) = BC.dirichlet.Vr_II; % apply far-field potential flow condition
-%     for ii = 2:(size(PHI,1)-1) % loop through radii
-%         V_n(ii,:) = (PHI(ii+1,:,iter) - PHI(ii-1,:,iter))./(2*dr); % central difference
-%     end
-%     
-%     q2_ij = U_n.^2 + V_n.^2;
-%     a2_ij = (1./M0.^2)-0.5*(gam-1).*(q2_ij - 1);
-%     M2_ij = q2_ij./a2_ij; % local Mach number
 end
 
 %% Post Process
@@ -271,13 +306,6 @@ a2_ij = (1./M0.^2)-0.5*(gam-1).*(q2_ij - 1);
 M2_ij = q2_ij./a2_ij; % local Mach number
 
 M_ij = sqrt(M2_ij);
-% if M0 == 0
-%     folder = 'incompressible';
-% elseif max(M_ij(:)) < 1.0
-%     folder = 'subsonic';
-% else
-%     folder = 'supersonic';
-% end
 
 folderName = ['M_' num2str(M0)];
 
